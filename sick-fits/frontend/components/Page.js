8758b4be5e86a7e652d4ef80702751715dbeab4a
vicmmont <vicmmonterroso@gmail.com>
2024-11-22 23:50:22 -0500

    routes and setup

---
+++ sick-fits/frontend/components/Page.js
@@ -0,0 +1,6 @@
+export default function Page({ children }) {
+  return <div>
+    <h2>I am the page component</h2>
+    {children}
+  </div>;
+}
---
+++ sick-fits/frontend/pages/products.js
@@ -0,0 +1,7 @@
+export default function IndexPage() {
+  return (
+    <div>
+      <p>Products!</p>
+    </div>
+  );
+}
